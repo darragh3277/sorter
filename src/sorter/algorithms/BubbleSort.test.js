78873e8179b10d8f925b1a85a5aa1ef1683e0ce6
Darragh <darragh3277@gmail.com>
2020-06-16 14:48:07 +0100

    add more tests to bubble sort

--- src/sorter/algorithms/BubbleSort.test.js
+++ src/sorter/algorithms/BubbleSort.test.js
@@ -1,8 +1,25 @@
 import BubbleSort from "./BubbleSort";
 
-const columns = [3, 2, 4, 6, 5, 7, 8, 1, 10, 9];
+const columns = [
+  {
+    input: [3, 2, 4, 6, 5, 7, 8, 1, 10, 9],
+    expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  },
+  { input: [2, 1], expected: [1, 2] },
+  { input: [2, 1, 5, 3, 4], expected: [1, 2, 3, 4, 5] },
+];
 
-test("array should be sorted", () => {
-  let sorter = new BubbleSort(columns);
-  expect(sorter.sort()).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+test.each(columns)("Arrays should be sorted", ({ input, expected }) => {
+  let sorter = new BubbleSort(input);
+  expect(sorter.sort()).toStrictEqual(expected);
+});
+
+test("Empty should return empty", () => {
+  let sorter = new BubbleSort([]);
+  expect(sorter.sort()).toStrictEqual([]);
+});
+
+test("Array of size 1 should return itself", () => {
+  let sorter = new BubbleSort([2]);
+  expect(sorter.sort()).toStrictEqual([2]);
 });
